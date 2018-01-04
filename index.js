34445699a21e7f9950a56c6053717eb5e47102ad
ericdrewes <eric.d.drewes@gmail.com>
2018-01-04 15:22:24 -0600

    update

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
-const passport = require('passport');
+const passport = require("passport");
 const strategy = require(`${__dirname}/strategy.js`);
-const express = require('express');
-const session = require('express-session');
-const request = require('request');
+const express = require("express");
+const session = require("express-session");
+const request = require("request");
 
 const port = 3000;
 
 const app = express();
 
-app.use( 
-	session({
-		secret: 'thisisasecret',
-		resave: false,
-		saveUnitialized: false
-	})
-)
+app.use(
+  session({
+    secret: "thisisasecret",
+    resave: false,
+    saveUnitialized: false
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(strategy);
 
-passport.serializeUser( (user, done) => {
-	const {_json} = user;
-	done(null, {clientID: _json.clientID, email: _json.email, name: _json.name, followers_url: _json.followers_url});
-})
+passport.serializeUser((user, done) => {
+  const { _json } = user;
+  done(null, {
+    clientID: _json.clientID,
+    email: _json.email,
+    name: _json.name,
+    followers_url: _json.followers_url
+  });
+});
 
-passport.deserializeUser( (obj, done) => {
-	done(null, obj);
-})
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 
-app.get('/login',
-	passport.authenticate('auth0', 
-		{successRedirect: '/followers', failureRedirect: '/login', failureFlash: true, connection: 'github'}
-	)
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    successRedirect: "/followers",
+    failureRedirect: "/login"
+  })
 );
 
-app.get('/followers', (req, res, next) => {
-	if(req.user) {
-		const followersRequest = {
-			url: req.user.followers,
-			headers: {
-				'User-Agent': req.user.clientID
-			}
-		}
-		request(followersRequest, (error, response, body) => {
-			res.status(200).send(body);
-		})
-	} else {
-		redirect('/login');
-		}
-})
+app.get("/followers", (req, res, next) => {
+  console.log(req.user);
+  if (req.user) {
+    const followersRequest = {
+      url: req.user.followers_url,
+      headers: {
+        "User-Agent": req.user.clientID
+      }
+    };
+    request(followersRequest, (error, response, body) => {
+      res.status(200).send(body);
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
 
-app.listen(port, () => { 
-	console.log(`Listening on port: ${port}`)
-})
-
-
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
--- strategy.js
+++ strategy.js
@@ -1,15 +1,16 @@
-const Auth0Strategy = require('passport-auth0');
+const Auth0Strategy = require("passport-auth0");
 const config = require(`${__dirname}/config.js`);
-const { domain, clientID, clientSecret } = require('./config');
+const { domain, clientID, clientSecret } = require("./config");
 
 module.exports = new Auth0Strategy(
-	{
-		domain: domain,
-		clientID: clientID,
-		clientSecret: clientSecret,
-		callbackURL: '/login'
-	},
-	function(accessToken, refreshToken, extraParams, profile, done) {
-		return done(null, profile);
-	}
+  {
+    domain: domain,
+    clientID: clientID,
+    clientSecret: clientSecret,
+    callbackURL: "/login",
+    scope: "openid profile user"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
+  }
 );
